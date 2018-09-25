24f060b7fb7fab2d0591e1f4c323b0e3a8377293
wanyong117 <wanyong117@gmail.com>
2018-09-25 17:20:42 +0800

    0925

--- FormatParseLatex.py
+++ FormatParseLatex.py
@@ -27,6 +27,7 @@ def formatParseLatex4Code(strLatex,Variables):
     return leftEquation,rightEquation,variableName
     
 def format4Design(strLatex):
+  
     strResult=formatSinglePart(strLatex)
     strResult=formatVariableName(strResult)
     strResult=formatDot(strResult)
@@ -49,37 +50,27 @@ def formatSinglePart(strLatex):
     return strEquation
 
 def formatDot(strEquation):
-    p=standardre.compile("(dot\*)([e-zE-Z0-9][a-zA-Z0-9]*)([^a-zA-Z0-9]|$)")
+    p=standardre.compile("(dot\*)([a-zA-Z0-9][a-zA-Z0-9]*)([^a-zA-Z0-9]|$)")
     if standardre.search(p, strEquation ):
         return p.sub(r"diff(\2)\3",strEquation)
-    p=standardre.compile("([a-dA-D][a-zA-Z0-9]*)(\*dot)")
+    p=standardre.compile("([a-dA-Z][a-zA-Z0-9]*)(\*dot)")
     return p.sub(r"diff(\1)",strEquation)
+
 
 def formatVariableName(strEquation):
      p=standardre.compile(r'([a-zA-Z0-9])(_\{)([a-zA-Z0-9])(\})')
      return p.sub(r'\1\3',strEquation)
 
-def formatSympify(strEquation):
-    p=standardre.compile(r'([^\+\=\*/]*)([\+\=\*/])([^\+\=\*/]*)')
-    strResult=''
-    needMoreSympify=False
-    AddOneSympify=False
-    for tmp in p.split(strEquation):
-        if "(" in tmp:
-            tmp='S.sympify(\''+tmp+'\')'
-            AddOneSympify=True 
-        elif not tmp:
-            needMoreSympify=True
-        strResult+=tmp
-    if needMoreSympify:
-        strResult='S.sympify(\''+strResult+'\')' 
-    elif not AddOneSympify:        
-        strResult='S.sympify(\''+strResult+'\')' 
-        
-    #if "(" in strEquation:
-    #    strResult='S.sympify(\''+strResult+'\')' 
-    return strResult
 def formatContinuourVariable(strEquation,Variables):
+    #p=standardre.compile(r'(\()(x|y|t|pt)(\))')
+    #strParttern=
+    p=standardre.compile(r'(^|[^a-zA-Z0-9])('+'|'.join(Variables)+r')($|[^a-zA-Z0-9])')
+    #return p.sub(r'\1\2(t)\3',strEquation)
+    if "diff" in strEquation:        
+        return p.sub(r"\1\2(t)\3",strEquation)
+    else:            
+        return p.sub(r"\1S.sympify('\2(t)')\3",strEquation)
+def formatContinuourVariable_old(strEquation,Variables):
     #p=standardre.compile(r'(\()(x|y|t|pt)(\))')
     #strParttern=
     p=standardre.compile(r'(\()('+'|'.join(Variables)+r')(\))')
@@ -90,6 +81,64 @@ def getContinuousVariable(strEquation,Variables):
     searchG=standardre.search(p, strEquation )
     if searchG:
         return searchG.group(2)
+    else:        
+        return ''; 
+    
+def formatSympifySubEquation(strEquation):
+    sympyEquation=['exp','sum','prod','log','ln','sin','cos','tan','csc','sec',
+                   'cot','arcsin','arccos','arctan','arccsc','arcsec','arccot',
+                   'sinh','cosh','tanh','arcsinh','arccosh','arctanh','sqrt',
+                   'times','cdot','div','frac','mathit'
+                   ]
+      
+
+    p=standardre.compile(r'(^|[^a-zA-Z0-9])('+'|'.join(sympyEquation)+r')($|[^a-zA-Z0-9])')   
+    return p.sub(r'\1S.\2\3',strEquation)
+def formatSympify(strEquation):
+    
+    
+    p=standardre.compile(r'^([0-9.]+)$')
+    if (p.match(strEquation)):   
+        return 'S.sympify(\''+strEquation+'\')'    
+    p=standardre.compile(r'^([a-zA-Z0-9]+)$')
+    if (p.match(strEquation)):   
+        return 'S.sympify('+strEquation+')'
+    p=standardre.compile(r'^([\(\)a-zA-Z0-9]+)$')
+    if (p.match(strEquation)):   
+        return 'S.sympify(\''+strEquation+'\')'   
+    else: 
+        return 'S.sympify('+formatSympifySubEquation(strEquation)+')'
+
+def formatSympify_old(strEquation):
+    isMustAddSym=False
+    p=standardre.compile(r'(\()([^()]*)(\))')
+    p2=standardre.compile(r'([\w]*)(\()([^()]*)(\))')
+    p3=standardre.compile(r'([\w]*)(\()([^()]*)(\))')
+    strTmp=strEquation#p.sub(r'#\2$',strEquation)
+    if (not p3.match(strTmp)):
+        isMustAddSym=True
     else:
-        
-        return '';
+        isMustAddSym=False
+    strTmp=p2.sub(r"S.sympify#'\1#\3$'$",strTmp)
+    while '(' in strTmp:            
+        if (not p3.match(strTmp)):
+            isMustAddSym=True
+        else:
+            isMustAddSym=False 
+        strTmp=p2.sub(r"S.sympify#\1#\3$$",strTmp)       
+    strTmp=strTmp.replace('#','(')
+    strTmp=strTmp.replace('$',')')    
+    if isMustAddSym:
+        if "S.sympify" in strTmp:
+            strTmp='S.sympify('+strTmp+')' 
+        else:
+            if not "(" in strTmp:
+                strTmp='S.sympify('+strTmp+')'
+            else:
+                strTmp='S.sympify(\''+strTmp+'\')'
+    return strTmp
+ 
+def formatBlockName(strLine):
+    p=standardre.compile(r'Block Begin (.*)\$')
+    return p.search(strLine).group(1)
+    
